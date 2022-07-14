fdcc9fe5b61a3d033372a01d636e6da0a5601283
aahuja07 <108749000+aahuja07@users.noreply.github.com>
2022-07-14 11:54:34 -0400

    computer choice
    random comp choice

--- game.py
+++ game.py
@@ -1,3 +1,24 @@
+import random #loading random module
+
 print("Rock, Paper, Scissors, Shoot!")
-input()
-my name is anmol
+# USER INPUT
+user_selection = input("Please select 'rock', 'paper' or 'scissors'!") 
+print("You chose:", user_selection)
+#VALIDATE USER INPUT
+
+#COMPUTER CHOICE
+
+options = ["rock", "paper", "scissors"]
+computer_choice = random.choice(options)
+print("Computer chose:", computer_choice)
+
+
+#WINNER
+
+
+
+
+
+
+
+#RESULT
