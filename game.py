160c142dd5440428197d27d366581c7208f54a4f
aahuja07 <108749000+aahuja07@users.noreply.github.com>
2022-07-14 15:24:42 -0400

    winner selection

--- game.py
+++ game.py
@@ -14,8 +14,26 @@ print("Computer chose:", computer_choice)
 
 
 #WINNER
+if user_selection == computer_choice:
+    print("You both chose", user_selection, "It's a tie!")
 
+elif user_selection == "rock":
+    if computer_choice == "scissors":
+        print("Rock beats scissors. You win!")
+    else:
+        print("Paper beats rock. Computer wins!") 
 
+elif user_selection == "scissors":
+    if computer_choice == "paper":
+        print("Scissors beats Paper. You win!")
+    else:
+        print("Rock beats scissors. Computer wins!")
+        
+elif user_selection == "paper":
+    if computer_choice == "rock":
+        print("Paper beats Rock. You win!")
+    else:
+        print("Scissors beat Paper. Computer wins!")   
 
 
 
